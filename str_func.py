de466c177f819bde4c4ed6d347f34473fab4bee5
SergeyUpgrade <sergey.vasilev.98@mail.ru>
2024-05-21 22:11:55 +0300

    add docstring
    Надо больше docstring

--- str_func.py
+++ str_func.py
@@ -1,7 +1,7 @@
 def words_UP(value: str):
     """Было неправильно описано, теперь я правильно описал"""
     return value.upper()
-
+"""Еще вот здесь напишу"""
 value = input()
 
 print(f"{words_UP(value)}")
